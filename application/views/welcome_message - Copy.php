8fd8a0ae214c9ce47c6c44cd1fcf8198d1e7b933
karimulla <karimullsyed@notionpress.com>
2017-05-03 12:50:49 +0530

    testing

--- application/views/welcome_message - Copy.php
+++ application/views/welcome_message - Copy.php
@@ -68,6 +68,7 @@
 
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>
+	<h2>Welcome to CodeIgniter!</h2>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
